7ee781aeda8ce8fb5bfe587fd37cc6d73a839017
Duckers <duckers@outracks.net>
2017-07-29 23:52:17 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuse.Babel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = Fuse.Scripting.V8.Simple.Context.Create(Handle.Free, Handle.Free);
+
+            var result = context.Evaluate("lol", "2+2", new AutoReleasePool(), x => Console.WriteLine("ERROR"));
+            Console.WriteLine(result.AsInt());
+        }
+    }
+}
